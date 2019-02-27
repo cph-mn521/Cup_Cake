738e1940012101223b43e771053ff6bc3bd653ce
cph-mn521 <43202726+cph-mn521@users.noreply.github.com>
2019-02-27 15:34:53 +0100

    Update to UserDAO, Fixes 1.
    added table and column names. If an error is encountered, throw it up another level to be handled by the controller.

--- src/main/java/com/cupcakes/data/UserDAO.java
+++ src/main/java/com/cupcakes/data/UserDAO.java
@@ -17,53 +17,46 @@ import java.util.List;
  * @author Martin Wulff
  */
 public class UserDAO {
-    
+
     private DB connector = null;
     private Connection connection;
     private Statement stmt;
     private List<recipeDTO> recipes = new ArrayList();
     private List<ingredientDTO> ingredients;
-    
-    public UserDAO()throws SQLException{
+
+    public UserDAO() throws SQLException {
         this.connector = new DB();
         this.connection = this.connector.getConnection();
         this.stmt = this.connection.createStatement();
     }
-    
+
     /**
      * Method for extracting a user from the database.
-     * 
-     * @param Username  The desired username to be extracted from the database.
+     *
+     * @param Username The desired username to be extracted from the database.
      * @return User as a UserDTO.
      */
-    public UserDTO getUser(String Username){
-        UserDTO user = null;
+    public UserDTO getUser(String Username) throws SQLException {
+
         String Query = "SELECT * "
-                + "FROM CupCakes.Users"
-                + "Where Usnername="+Username
+                + "FROM cupcakes.User"
+                + "Where username=" + Username
                 + ";";
-        try {
-            ResultSet rs =stmt.executeQuery(Query);
-            while(rs.next()){ // User er en primary key. Kun 1 resultat bør returnes.
-                user = new UserDTO(
-                    rs.getString("Username"),
+        
+            ResultSet rs = stmt.executeQuery(Query);
+            rs.next(); // User er en primary key. Kun 1 resultat bør returnes.
+            UserDTO user = new UserDTO(
+                    rs.getString("username"),
                     rs.getString("email"),
                     rs.getString("password"),
                     rs.getDouble("balance")
-                );
-                
-            }
-                    
+            );
+            
+            if(user.getName()== null) throw new SQLException("No result found.");
+            
             return user;
-        }
-        catch(SQLException e){
-            //Returner et relevant svar. Evt kast fejlen længere op så den kan håndteres i controlleren.
-            return null;
-        }
-        
-        
-        
+
+
     }
-    
-    
+
 }
