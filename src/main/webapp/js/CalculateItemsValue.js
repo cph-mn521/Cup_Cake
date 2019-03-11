74a78a6b647902cd2fa4f2f4f4602914cbb83681
cph-ms782 <cph-ms782@cphbusiness.dk>
2019-03-11 10:29:08 +0100

    js_rettelser

--- src/main/webapp/js/CalculateItemsValue.js
+++ src/main/webapp/js/CalculateItemsValue.js
@@ -1,7 +1,7 @@
 function CalculateItemsValue() {
 	var total = 0;
 	list = document.getElementsByClassName("price");
-	for (price : list){
+	for (price in list){
 		total += price;
 	}
 	document.getElementById("TotalPrice").innerHTML = "$" + total;
